c7ecbf521738299365f0e482daa2b71307f980e2
EC2 Default User <ec2-user@ip-172-31-20-22.eu-north-1.compute.internal>
2023-12-27 19:41:05 +0000

    adding dockerfile

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+import uvicorn
+from nlplogic.corenlp import get_phrases
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello NLP"}
+    
+@app.get("/wikiphrases/{name}")
+async def wikiphrase(name: str):
+    """Generates NLP noun phrases from wiki name"""
+    
+    noun_phrases = get_phrases(name)
+    return {"phrases": noun_phrases}
+    
+if __name__ == '__main__' :
+    uvicorn.run(app, port=8080, host='0.0.0.0')
